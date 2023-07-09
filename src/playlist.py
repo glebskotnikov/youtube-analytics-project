43c43efd3295b718cc5e62b6ff084eb413de530c
Gleb Skotnikov <skotnikov.nsk@gmail.com>
2023-07-09 18:45:02 +0700

    Add new code (homework_5)

--- src/playlist.py
+++ src/playlist.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 import isodate
@@ -8,30 +9,37 @@ from src.channel import Channel
 class PlayList:
     def __init__(self, playlist_id):
         youtube = Channel.get_service()
-        playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
-                                                       part='contentDetails, snippet',
-                                                       maxResults=50,
-                                                       ).execute()
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
-        self.video_response = youtube.videos().list(part='contentDetails,statistics',
-                                                    id=','.join(video_ids)
-                                                    ).execute()
-        self.title = playlist_videos['items'][0]['snippet']['title'][:24]
+        playlist = youtube.playlists().list(id=playlist_id,
+                                            part='contentDetails, snippet',
+                                            maxResults=50,
+                                            ).execute()
+        self.title = playlist['items'][0]['snippet']['title']
         self.url = 'https://www.youtube.com/playlist?list=' + playlist_id
+        self.playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
+                                                            part='contentDetails, snippet',
+                                                            maxResults=50,
+                                                            ).execute()['items']
 
     @property
     def total_duration(self):
-        total = isodate.parse_duration('PT0M0S')
-        for video in self.video_response['items']:
-            # YouTube video duration is in ISO 8601 format
+        youtube = Channel.get_service()
+        total = datetime.timedelta()
+        for pl_video in self.playlist_videos:
+            video = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                          id=pl_video['contentDetails']['videoId']
+                                          ).execute()['items'][0]
             iso_8601_duration = video['contentDetails']['duration']
             duration = isodate.parse_duration(iso_8601_duration)
             total += duration
         return total
 
     def show_best_video(self):
+        youtube = Channel.get_service()
         likes_count = 0
-        for video in self.video_response['items']:
+        for pl_video in self.playlist_videos:
+            video = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                          id=pl_video['contentDetails']['videoId']
+                                          ).execute()['items'][0]
             if int(video['statistics']['likeCount']) > likes_count:
                 likes_count = int(video['statistics']['likeCount'])
                 video_id = video['id']
