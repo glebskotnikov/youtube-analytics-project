6cbc2d62b2892b94af924f373e84e1c1f3f9f732
Gleb Skotnikov <skotnikov.nsk@gmail.com>
2023-07-11 16:29:27 +0300

    Add new code (homework_6)

--- src/video.py
+++ src/video.py
@@ -6,14 +6,21 @@ class Video:
     def __init__(self, video_id):
         """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
         youtube = Channel.get_service()
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
-        self.video_id = video_id
-        self.title = video_response['items'][0]['snippet']['title']
-        self.url = 'https://www.youtube.com/watch?v=' + video_id
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id
+                                                   ).execute()
+            self.video_id = video_id
+            self.title = video_response['items'][0]['snippet']['title']
+            self.url = 'https://www.youtube.com/watch?v=' + video_id
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_id = video_id
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __repr__(self):
         return f'{self.__class__.__name__}({self.video_id})'
